471dd34ac0b0767885562a0ba0719c8e14eccb10
mactar27 <mactar27@github.com>
2026-08-17 21:09:18 +0000

    chore: authorize utfs.io for images

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
